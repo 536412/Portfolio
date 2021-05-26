72a146454b6fbbe8be964738ad20dc5fcef6fd1d
Alper Aydinhan <536412@student.glu.nl>
2021-05-26 12:48:39 +0200

    added blank target tags at the anchors

--- src/components/content/projects/projects.jsx
+++ src/components/content/projects/projects.jsx
@@ -8,11 +8,11 @@ const Projects = () =>{
                 <h1>My Past projects</h1>
                 <div className="pPanel">
                     <div id="runaway"></div>
-                    <p><a href="https://github.com/536412/runaway">Super Runaway with JS!</a></p>
+                    <p><a target="_blank" href="https://github.com/536412/runaway">Super Runaway with JS!</a></p>
                 </div>
                 <div className="pPanel">
                     <div id="tictac"></div>
-                    <p><a href="https://github.com/536412/tictactoe">TicTacToe With JS!</a></p>
+                    <p><a target="_blank" href="https://github.com/536412/tictactoe">TicTacToe With JS!</a></p>
                 </div>
             </div>
         </div>
